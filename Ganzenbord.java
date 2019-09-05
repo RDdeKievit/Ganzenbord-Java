4f201c453102a86c409fdd0ba7ffd83c68949320
Your Name <you@example.com>
2019-09-05 14:53:40 +0200

    Functie dobbelsteen werkend

--- Ganzenbord.java
+++ Ganzenbord.java
@@ -1,6 +1,19 @@
+
+import java.util.Random;
 class Ganzenbord {
 	
+	static int worp;
+	
 	public static void main (String args[]) {
-		System.out.println("Hello");
+		
+		int[] dobbelsteen = {1, 2, 3, 4, 5, 6};
+		
+		worp = getRandom(dobbelsteen);
+		System.out.println(worp);
+	}
+	
+	public static int getRandom(int[] array) {
+   		int rnd = new Random().nextInt(array.length);
+		return array[rnd];
 	}
 }
