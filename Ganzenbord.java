6078ef7bb0e5dd96db7cc2358ed30b136c2d2983
Your Name <you@example.com>
2019-09-05 16:42:50 +0200

    Tweede speler succesvol geintegreerd

--- Ganzenbord.java
+++ Ganzenbord.java
@@ -3,15 +3,24 @@ import java.util.Random;
 class Ganzenbord {
 	
 	//globale variabelen; worden gebruikt door meerdere functies
+	private static int beurt;
 	private static int worp;
 	private static int positie1;
+	private static int positie2;
 	
 
 	//binnenkomst functie main; roept functies applicatie op volgorde aan
 	public static void main (String[] args) {
+		setBeurt();
 		setWorp();
 		setPositie();
 		winCheck();
+	}
+
+	//functie setBeurt; veranderd status globale variabele beurt en print beurt
+	public static void setBeurt() {
+		beurt = getBeurt();
+		System.out.println(beurt);
 	}
 
 	//functie setWorp; veranderd status globale variable worp (dobbelsteen) en print worp
@@ -22,37 +31,56 @@ class Ganzenbord {
 		System.out.println(worp);
 	}
 	
-	// functie getRandom; haalt random getal uit meegkregen array en returnt het gekozen getal
-	public static int getWorp(int[] array) {
-   		int rnd = new Random().nextInt(array.length);
-		return array[rnd];
-	}
-	
 	//functie setPositie; veranderd status van globale variable positie1 en print positie1
 	public static void setPositie() {
-		positie1 = getPositie();
-		System.out.println(positie1);
-	}
-
-	//functie getPositie; retunt nieuwe positie speler op basis van huidige positie en worp
-	public static int getPositie(){
-		int positieNew;
-		if ((positie1 + worp) > 50) {
-			positieNew = 35;
-			return positieNew;
-		} else {
-			positieNew = positie1 + worp;
-			return positieNew;
+		if (beurt == 1) {
+			positie1 = getPositie(positie1);
+			System.out.println(positie1);
+		}else {
+			positie2 = getPositie(positie2);
+			System.out.println(positie2);
 		}
 	}
 
 	//functie winCheck; checkt of er een speler op positie 50 staat en print felicitatie wanneer;
 	//dat het geval is. In ander geval is de volgende speler aan de beurt
 	public static void winCheck() {
-		if (positie1 == 50) {
-			System.out.println("Gefeliciteerd Speler1, je hebt gewonnen!");
+		if (positie1 == 50 || positie2 == 50) {
+			System.out.println("Gefeliciteerd Speler"+beurt+", je hebt gewonnen!");
 		}else {
 			main(new String[0]);
+		}
+	}
+	
+	//get functies hieronder
+
+	//functie getBeurt; bepaalt welk van de twee spelers nu aan de beurt is en retunt beurtNew
+	public static int getBeurt() {
+		int beurtNew;
+		if ((beurt + 1) > 2) {
+			beurtNew = 1;
+			return beurtNew;
+		}else {
+			beurtNew = beurt + 1;
+			return beurtNew;
+		}
+	}
+
+	// functie getRandom; haalt random getal uit meegkregen array en returnt het gekozen getal
+	public static int getWorp(int[] array) {
+   		int rnd = new Random().nextInt(array.length);
+		return array[rnd];
+	}
+
+	//functie getPositie; retunt nieuwe positie speler op basis van huidige positie en worp
+	public static int getPositie(int positie) {
+		int positieNew;
+		if ((positie + worp) > 50) {
+			positieNew = 35;
+			return positieNew;
+		} else {
+			positieNew = positie + worp;
+			return positieNew;
 		}
 	}
 }
